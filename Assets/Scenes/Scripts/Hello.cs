0c6f42918736a815e8f3c17aec889ebe9e0e96af
Laurent Robidoux <laurent.robidoux.cem@gmail.com>
2019-11-07 15:16:49 -0500

    changed script folder

---
+++ Assets/Scenes/Scripts/Hello.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ohrizon.Dependancy
+{
+    public class Hello : MonoBehaviour
+    {
+        public int speed;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            this.transform.position = new Vector3(speed, 0, 0) + this.transform.position;
+        }
+    }
+}
